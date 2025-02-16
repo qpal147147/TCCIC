39cb32b12bf70b7a7b606ffdeb7cea204098cef4
qpal147147 <43807991+qpal147147@users.noreply.github.com>
2025-02-16 12:54:44 +0800

    [refactor] pipeline and parse

--- tccic/pipelines.py
+++ tccic/pipelines.py
@@ -6,8 +6,16 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
-
+from pathlib import Path
+from markdownify import markdownify as md
 
 class TccicPipeline:
     def process_item(self, item, spider):
+        html_filename = f"temp/output_{item['url'].split('/')[-2].replace('?','_')}.html"
+        mdt_filename = f"temp/output_{item['url'].split('/')[-2].replace('?','_')}.md"
+
+        Path(html_filename).write_bytes(item['body'])
+
+        md_text = md(item['text'])
+        Path(mdt_filename).write_text(md_text)
         return item
--- tccic/spiders/tccic_spider.py
+++ tccic/spiders/tccic_spider.py
@@ -1,5 +1,3 @@
-from pathlib import Path
-from markdownify import markdownify as md
 import scrapy
 
 class TccicSpider(scrapy.Spider):
@@ -7,17 +5,11 @@ class TccicSpider(scrapy.Spider):
     
     def __init__(self, url=None, *args, **kwargs):
         super(TccicSpider, self).__init__(*args, **kwargs)
-        self.url = url
-
-    def start_requests(self):
-        yield scrapy.Request(url=self.url, callback=self.parse)
+        self.start_urls = [url]
 
     def parse(self, response):
-        html_filename = f"output_{response.url.split('/')[-2].replace('?','_')}.html"
-        mdt_filename = f"output_{response.url.split('/')[-2].replace('?','_')}.md"
-
-        Path(html_filename).write_bytes(response.body)
-        self.log(f"已儲存檔案 {html_filename}")
-
-        md_text = md(response.text)
-        Path(mdt_filename).write_text(md_text)
+        item = {}
+        item['url'] = response.url
+        item['body'] = response.body
+        item['text'] = response.text
+        yield item
