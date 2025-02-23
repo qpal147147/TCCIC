e741cb46a7db76669021f2f7014a697f0b0455c4
qpal147147 <43807991+qpal147147@users.noreply.github.com>
2025-02-23 22:01:46 +0800

    [Feature] Add recrawl and clean api

--- api.py
+++ api.py
@@ -1,10 +1,12 @@
 import subprocess
-from typing import Union
+import shutil
+from pathlib import Path
 
 from fastapi import FastAPI, Form
 from fastapi.responses import JSONResponse
 
 from utils.config_utils import get_bank_and_card_name
+from rag import RAG
 
 config_path = './card.yaml'
 
@@ -29,25 +31,49 @@ async def start_spider(url: str, bank_code: str):
 
 app = FastAPI()
 
-# Test connection
 @app.get("/")
 async def welcome():
     return {"Hello World"}
 
 
-# Do RAG from crawled data
 @app.post("/rag")
 async def start_rag(url: str = Form(...), bank_code: str = Form(...)):
-    spider_status = await start_spider(url, bank_code)
-    if not spider_status:
-        return JSONResponse(content={"status": "error", "msg": "An error occurred during the crawl."}, status_code=400)
-    
-
+    # get bank and card name
     bank_name, card_name = get_bank_and_card_name(config_path, bank_code, url)
     if not bank_name or not card_name:
         return JSONResponse(content={"status": "error", "msg": "No bank or card found."}, status_code=400)
     
+    # check if json file exists
+    json_path = f"./json_data/{bank_name}/{card_name}/data.json"
+    if not Path(json_path).exists():
+        spider_status = await start_spider(url, bank_code)
+        if not spider_status:
+            return JSONResponse(content={"status": "error", "msg": "An error occurred during the crawl."}, status_code=400)
+    
+    # do rag
+    response = RAG(json_path).complete()
+    
     return JSONResponse(content={"status": "success", "data": {"bank": bank_name, "card": card_name}}, status_code=200)
+
+
+@app.post("/recrawl")
+async def recrawl(url: str = Form(...), bank_code: str = Form(...)):
+    spider_status = await start_spider(url, bank_code)
+    if not spider_status:
+        return JSONResponse(content={"status": "error", "msg": "An error occurred during the crawl."}, status_code=400)
+    
+    return JSONResponse(content={"status": "success", "msg": "Recrawl Successfully"}, status_code=200)
+
+
+@app.post("/clean")
+async def clean():
+    data_folder = Path("./json_data")
+    if data_folder.exists():
+        shutil.rmtree(data_folder)
+    data_folder.mkdir(parents=True, exist_ok=True)
+
+    return JSONResponse(content={"status": "success", "msg": "Clean Successfully"}, status_code=200)
+
 
 
 if __name__ == "__main__":
