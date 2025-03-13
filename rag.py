1aa36596d4d8bd6a5f7c4799752589d968985d06
qpal147147 <43807991+qpal147147@users.noreply.github.com>
2025-03-14 01:46:32 +0800

    [Feat] Improve RAG

--- rag.py
+++ rag.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 from llama_index.core import Document, StorageContext
 from llama_index.core import Settings
 from llama_index.core import VectorStoreIndex
-from llama_index.core.node_parser import MarkdownNodeParser
+from llama_index.core.node_parser import MarkdownNodeParser, SimpleFileNodeParser
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 from llama_index.vector_stores.lancedb import LanceDBVectorStore
 from llama_index.llms.ollama import Ollama
@@ -20,11 +20,18 @@ class RAG():
         self.card_name = self.json_data['card']
         self.last_update = self.json_data['data']
         self.lancedb_path = f"./data/{self.bank_name}/{self.card_name}/lancedb"
+        self._prompt = """
+        以下為文章內容，請總結此篇文章的內容，並限制500字以內。不要使用"總結如下"等開頭，回傳總結的文字內容就好。
+
+        文章內容:
+
+        {content}
+        """
 
         load_dotenv()
 
         # create models
-        Settings.llm = Gemini(model="models/gemini-2.0-flash-lite")
+        Settings.llm = Gemini(model="models/gemini-2.0-flash-lite", temperature=0.5)
         # Settings.llm = Ollama(model="cwchang/llama3-taide-lx-8b-chat-alpha1:q4_k_s", request_timeout=300.0)
         Settings.embed_model = HuggingFaceEmbedding(model_name="intfloat/multilingual-e5-large")
 
@@ -39,22 +46,28 @@ class RAG():
     
     def embedding(self):
         try:
-            md_parser = MarkdownNodeParser()
+            # md_parser = MarkdownNodeParser()
+            parser = SimpleFileNodeParser()
             pages = self.json_data['pages']
 
             docs = []
             for page in pages:
                 md_text = md(page['html_content'], strip=['a', 'img'])
+                response = Settings.llm.complete(self._prompt.format(content=md_text))
+
                 doc = Document(
-                    text=md_text, 
-                    extra_info={'url': page['url']}, 
+                    text=response.text, 
+                    extra_info={
+                        'url': page['url'],
+                        'original_text': md_text,
+                    }, 
                     excluded_llm_metadata_keys=["url"],
-                    excluded_embed_metadata_keys = ["url"],
+                    excluded_embed_metadata_keys = ["url", "original_text"],
                     id_=str(uuid.uuid4()),
                 )
                 docs.append(doc)
                 
-            nodes = md_parser.get_nodes_from_documents(docs, show_progress=True)
+            nodes = parser.get_nodes_from_documents(docs, show_progress=True)
             
             # create vector store and save index
             vector_store = LanceDBVectorStore(uri=self.lancedb_path, mode="overwrite")
@@ -99,8 +112,9 @@ class RAG():
         # format response
         json_data["source_data"] += [
             {
-                # "text": source_node.node.text, 
+                "text": source_node.node.text,
                 "score": source_node.score,
+                # "source_text": source_node.metadata['original_text'],
                 "url": source_node.metadata['url'],
             } 
             for source_node in response.source_nodes
